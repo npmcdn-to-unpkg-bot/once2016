666cc01c33ec102a720327cc82456e9ce2f76214
tobe <tobeg3oogle@gmail.com>
2016-08-20 18:05:24 +0800

    Implement basic index API

--- website/home/views.py
+++ website/home/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+
+import logging
+import json
+import datetime
+
+logging.basicConfig(level=logging.DEBUG)
+
+def index(request):
+    return HttpResponse("Welcome to Once studio")
