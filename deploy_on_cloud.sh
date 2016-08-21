e63c3969914a2cf85bff949434be6b4ff2d6d8df
tobe <tobeg3oogle@gmail.com>
2016-08-21 22:24:49 +0800

    Add python dist files

--- deploy_on_cloud.sh
+++ deploy_on_cloud.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+set -x
+set -e
+
 sudo apt-get install -y python python-pip
 
 git clone https://github.com/tobegit3hub/once2016.git
 
+cd ./once2016/
+
+pip install -r requirements.txt
+
+python ./website/manage.py runserver 0.0.0.0:80
