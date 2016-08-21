58ddf29b8c24be4c2b0ad28cc2eddc413016f59b
tobe <tobeg3oogle@gmail.com>
2016-08-21 13:31:29 +0800

    Seperate header and footer html

--- website/home/views.py
+++ website/home/views.py
@@ -14,3 +14,7 @@ def index(request):
     context = {}
     return render(request, 'index.html', context)
     #return HttpResponse("Welcome to Once studio")
+
+def about(request):
+	context = {}
+	return render(request, 'about.html', context)
