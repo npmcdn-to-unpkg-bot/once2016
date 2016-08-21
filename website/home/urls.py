aa2663399855d792a8ff98ea318af646efcdd24c
tobe <tobeg3oogle@gmail.com>
2016-08-21 18:00:52 +0800

    Remove the useless parameter and internal page for getpost page

--- website/home/urls.py
+++ website/home/urls.py
@@ -9,6 +9,5 @@ urlpatterns = [
     url(r'^about/$', views.about, name='about'),
     url(r'^photos/$', views.photos, name='photos'),
     url(r'^getphoto/$', views.getphoto, name='getphoto'),
-    url(r'^(?P<question_id>[0-9]+)/action/$', views.getphoto_action, name='getphoto_action'),
-    url(r'^(?P<question_id>[0-9]+)/result/$', views.getphoto_result, name='getphoto_result'),
+    url(r'^getphoto/result/$', views.getphoto_result, name='getphoto_result'),
 ]
--- website/home/views.py
+++ website/home/views.py
@@ -33,40 +33,16 @@ def getphoto(request):
 	return render(request, 'getphoto.html', context)
 
 
-def getphoto_action(request, question_id):
-    #question = get_object_or_404(Question, pk=question_id)
-    try:
-    	pass
+def getphoto_result(request):
 
-        #selected_choice = question.choice_set.get(pk=request.POST['choice'])
-    except (KeyError, Choice.DoesNotExist):
-    	pass
-    	'''
-        # Redisplay the question voting form.
-        return render(request, 'polls/detail.html', {
-            'question': question,
-            'error_message': "You didn't select a choice.",
-        })
-        '''
-      
-    else:
-    	return HttpResponseRedirect(reverse('home:getphoto_result', args=(question_id,)))
-    	'''
-        selected_choice.votes += 1
-        selected_choice.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
-        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
-        '''
+    #request.POST['choice']
+    question_id = "10"
 
-
-def getphoto_result(request, question_id):
 	#def results(request, question_id):
     #question = get_object_or_404(Question, pk=question_id)
     #return render(request, 'polls/results.html', {'question': question})
-    if question_id == 11:
-        context = {"message": "ok, get your image"}
+    if question_id == "10":
+        context = {"message": "ok, get your image", "question_id": question_id}
     else:
-        context = {"message": "you can not get the image"}
+        context = {"message": "you can not get the image!!!", "question_id": question_id}
     return render(request, 'getphoto_result.html', context)
